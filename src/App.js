b76ce424f425685f84aeaea02a7d00b205e64ea4
ARSHADAMEEN00 <ameen.osperb@gmail.com>
2022-11-20 11:47:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <h1>hello world</h1>
+      <>by osperb</>
+    </div>
+  );
+}
+
+export default App;
